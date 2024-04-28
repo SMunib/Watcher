b255d85805785876b87ba7ed5d2476a18710497a
SMunib <munibrehman10@gmail.com>
2024-04-28 21:37:01 +0500

    login and register complete

--- app.py
+++ app.py
@@ -1,37 +1,15 @@
-from flask import Flask,redirect,request
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash,check_password_hash
+from flask import Flask
+from models import db
+from routes import auth_bp
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-db = SQLAlchemy(app)
-
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(200), nullable = False, unique = True)
-    email = db.Column(db.String(200), unique = True, nullable = True)
-    password = db.Column(db.String(200), nullable = False)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
     
-    def __repr__(self):
-        return f"User('{self.username},'{self.email}')"
-    
-    def set_password(self,password):
-        self.password = generate_password_hash(password)
+app.register_blueprint(auth_bp,url_prefix='/api')
         
-    def check_password(self,password):
-        return check_password_hash(self.password,password)
-    
-@app.route('/register')
-def register():
-    data = request.get_json()
-    new_user = Users(username = data.get('username'),password = data.get('password'),email = data.get('email'))
-    
-    try:
-        db.session.add(new_user)
-        db.session.commit()
-        return 'User successfully registered', 201 
-    except Exception as e:
-        return str(e), 500
-    
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
---
+++ routes.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, request,jsonify
+from models import db,Users
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/register',methods = ['POST'])
+def register():
+    data = request.get_json()
+    new_user = Users(username = data.get('username'),email = data.get('email'))
+    new_user.set_password(data.get('password'))
+    
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        data = {
+            "message":"User Successfully Registered!",
+            "status": 200
+        }
+        return jsonify(data)
+    except Exception as e:
+        db.session.rollback()
+        error = {
+            "message": str(e),
+            "status": 500
+        }
+        return jsonify(error)
+
+@auth_bp.route('/', methods = ['Get','POST'])
+def login():
+    if request.method == 'POST':
+        data = request.get_json()
+        email = data.get('email')
+        password = data.get('password')
+        
+        try:
+            user = Users.query.filter_by(email=email).first_or_404()
+            if user.check_password(password):
+                return 'Login Successful!',200
+            else:
+                raise ValueError('Invalid Username or Password')
+        except Exception as e:
+            return str(e), 401
+    else:
+        return "Hello"
