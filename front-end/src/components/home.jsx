569ee4cd13709b1e59871195889b82768683cf36
asim-5 <asimn6062@gmail.com>
2024-05-04 03:09:34 +0500

    navbar done

--- front-end/src/components/home.jsx
+++ front-end/src/components/home.jsx
@@ -1,7 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import { Link, useNavigate } from 'react-router-dom';
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
+import logo from '../assets/netflix1.png';
 
-export default function home() {
+
+export default function Home() {
+  const navigate = useNavigate();
+  const [expand, updateExpanded] = useState(false);
+  const [navColour, updateNavbar] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  function scrollHandler() {
+    if (window.scrollY >= 20) {
+      updateNavbar(true);
+    } else {
+      updateNavbar(false);
+    }
+  }
+
+  window.addEventListener('scroll', scrollHandler);
+
+  const handleSearchInputChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSearch = () => {
+    // Perform search logic here
+    console.log('Searching for:', searchQuery);
+  };
+
   return (
-    <div>home</div>
-  )
+    <div className="mainn">
+      <Navbar
+        expanded={expand}
+        fixed="top"
+        expand="md"
+        className={navColour ? 'sticky' : 'navbar'}
+      >
+        <Container>
+          <Navbar.Brand as={Link} to="/" className="d-flex">
+            <img src={logo} className="img-fluid logo" alt="brand" />
+          </Navbar.Brand>
+
+          <Navbar.Toggle
+            aria-controls="responsive-navbar-nav"
+            onClick={() => {
+              updateExpanded(expand ? false : 'expanded');
+            }}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto" defaultActiveKey="#home">
+              <Nav.Item className="search-bar">
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  value={searchQuery}
+                  onChange={handleSearchInputChange}
+                />
+                <button className="search-button" onClick={handleSearch}>
+                  Search
+                </button>
+              </Nav.Item>
+              
+              <Nav.Item>
+                <Nav.Link
+                  as={Link}
+                  to="/about"
+                  onClick={() => updateExpanded(false)}
+                >
+                  <AiOutlineUser style={{ marginBottom: '2px' }} /> About
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
