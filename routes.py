8e61e1cb3c95987123cb08e4aa3237467ca3e538
SMunib <munibrehman10@gmail.com>
2024-05-03 23:06:02 +0500

    more routes created

--- routes.py
+++ routes.py
@@ -51,12 +51,78 @@ def login():
     else:
         return "Hello"
     
-@auth_bp.route('/select',methods = ['GET'])
+@auth_bp.route('/select',methods = ['GET','Post'])
 def displaygenres():
-    genres = db.session.query(Movies.Genres).all()
-    unique_genres = set()
-    for genres_str in genres:
-        genres = genres_str[0].split(', ')
-        unique_genres.update(genres)
-    unique_genres.discard('') 
-    return jsonify(list(unique_genres))
+    if request.method == 'Get':
+        try:
+            genres = db.session.query(Movies.Genres).all()
+            unique_genres = set()
+            for genres_str in genres:
+                genres = genres_str[0].split(', ')
+                unique_genres.update(genres)
+            unique_genres.discard('') 
+            return jsonify(list(unique_genres))
+        except Exception as e:
+            return jsonify({"error": str(e)})
+    else:
+        genres_list = request.json.get('genres')
+        response = {}
+        try:
+            for genre in genres_list:
+                movies = Movies.query.filter(Movies.Genres.contains(genre)).order_by(Movies.AvgVote.desc()).limit(3).all()
+                movie_list = []
+                for movie in movies:
+                    movie_list.append({
+                        'title':movie.Title,
+                        'genres':movie.Genres,
+                        'rating':movie.AvgVote
+                    })
+                response[genre] = movie_list
+            return jsonify(response)
+        except Exception as e:
+            return jsonify({"error": str(e)})
+
+@auth_bp.route('/movies',methods = ['POST','GET'])
+def displaymovies():
+    #Category wise display of movies
+    if request.method == 'POST':
+        genre = request.json.get('genre')
+        try:
+            movies = Movies.query.filter(Movies.Genres.contains(genre)).all()
+            movie_list = []
+            for movie in movies:
+                movie_list.append({
+                    'title':movie.Title
+                })
+            return jsonify(movie_list)
+        except Exception as e:
+            return jsonify({"error":str(e)})
+    else:
+        #Top rated movies
+        try:
+            movies = Movies.query.order_by(Movies.AvgVote.desc()).limit(50).all()
+            movie_list = []
+            for movie in movies:
+                movie_list.append({
+                    'title':movie.Title,
+                    'rating':movie.AvgVote
+                })
+            return jsonify(movie_list)
+        except Exception as e:
+            return jsonify({"error":str(e)})
+
+@auth_bp.route('/search',methods = ['POST'])
+def searchmovie():
+    title = request.json.get('title')
+    try:
+        movies = Movies.query.filter(Movies.Title.like(f"%{title}%")).limit(5).all()
+        movie_list = []
+        for movie in movies:
+            movie_list.append({
+                'title':movie.Title,
+                'rating':movie.AvgVote
+            })
+        return jsonify(movie_list)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    
