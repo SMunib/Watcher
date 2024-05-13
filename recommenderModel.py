00d3aa01b9d8abf736cce636d87058b003984b3a
TM-3352 <k213352@nu.edu.pk>
2024-05-14 02:04:34 +0500

    Model Complete

--- recommenderModel.py
+++ recommenderModel.py
@@ -58,7 +58,7 @@ def recommendMoives(userID):
     allAvgRatings = []
     allMovieIDs = []
     numClusters = 4
-    acceptingSimilarity = 0.0
+    acceptingSimilarity = 0.015
     #
 
 
@@ -73,7 +73,6 @@ def recommendMoives(userID):
         dbCursor.execute("SELECT favGenres FROM users WHERE id = ?;", (userID, ))
         favGenres = dbCursor.fetchall()
         favGenres = convertTupleToList(favGenres)
-        print(favGenres)
 
         if favGenres:
             dbCursor.execute("SELECT MovieID, Genres, Title, AvgVote, VoteCount FROM movies WHERE VoteCount > ? ORDER BY AvgVote DESC", (minimumVotes, ))
@@ -86,7 +85,6 @@ def recommendMoives(userID):
                     favGenresMovies = row[1].split(", ")
 
                     if set([genre]).issubset(set(favGenresMovies)) and (row[3] > minAVColdStart): 
-                        # print(row[0])
                         if not set([row[0]]).issubset(set(recommendedMovies)):
                             recommendedMovies.append(row[0])
                             count += 1
@@ -106,7 +104,6 @@ def recommendMoives(userID):
 
         # iterating for each movie Collaborative then Content based filtering and generating recommendations based on each movie
         for movie in userWatchedMovies:
-            # print(movie)
             clusteringCoordinates = []
 
             # Getting the rating user has given to it
@@ -123,6 +120,7 @@ def recommendMoives(userID):
 
             # Creating coordinate for user movie
             clusteringCoordinates.append((X, Y))
+            allMovieIDs.append(movie)
 
             # Now, getting coordinates for all movies with similar genres
             dbCursor.execute("SELECT Genres FROM movies WHERE MovieID = ?", (movie, ))
@@ -142,7 +140,6 @@ def recommendMoives(userID):
 
                     # Getting highest rated n movies for the genre
                     if set([genre]).issubset(set(moviesGenres)) and not(row[0] in userWatchedMovies) :
-                        # print(row[3])
                         allRelativeRatings.append(relativeRating(row[3], userMovieRating))
                         allAvgRatings.append(row[3])
                         allMovieIDs.append(row[0])
@@ -151,14 +148,20 @@ def recommendMoives(userID):
             for x in range(len(allRelativeRatings)):
                 X = scaleRelativeRating(min(allRelativeRatings), max(allRelativeRatings), allRelativeRatings[x], 10)
                 Y = allAvgRatings[x]
-                clusteringCoordinates.append((X, Y))
+                clusteringCoordinates.append((X, Y))        
+            
+            # Converting coordinates from list of tuples to 2D numpy array
+            clusteringCoordinates = np.array(clusteringCoordinates)
 
             
-            plt.figure(figsize=(8, 6))
+            # plt.figure(figsize=(8, 6))
+            plt.scatter(clusteringCoordinates[:, 0], clusteringCoordinates[:, 1], color='blue', label='Movies from same genre')
+            plt.scatter(clusteringCoordinates[0][0], clusteringCoordinates[0][1], color='red')
+            # plt.annotate('User Movie', (clusteringCoordinates[0][0], clusteringCoordinates[0][1]), textcoords="offset points", xytext=(0, 10), ha='center')
+            # print(clusteringCoordinates[0])
+            
 
-            clusteringCoordinates = np.array(clusteringCoordinates)            
-            plt.scatter(clusteringCoordinates[:, 0], clusteringCoordinates[:, 1], color='blue', label='Original')
-
+            # Applying KMeans on cluster coordinates created
             kmeans = KMeans(n_clusters = numClusters)
             kmeans.fit(clusteringCoordinates)
             clusterCenters = kmeans.cluster_centers_
@@ -177,42 +180,233 @@ def recommendMoives(userID):
                     if np.array_equal(clusteringCoordinates[0], j):
                         movieClusterNo = i
 
-            # print(movieClusterNo)
+            # print(movieClusterNo + 1)
             clusterIndices = np.where(kmeans.labels_ == movieClusterNo)[0]
             clusterPoints = clusteringCoordinates[clusterIndices]
+            moviesSelected = [allMovieIDs[i] for i in clusterIndices]
+            # print(moviesSelected)
             # print(kmeans.labels_)
 
-            plt.title('K-means Clustering')
-            plt.xlabel('X-coordinate')
-            plt.ylabel('Y-coordinate')
+            # plt.title('K-Means Clustering')
+            # plt.xlabel('X-coordinate')
+            # plt.ylabel('Y-coordinate')
+            # plt.legend()
             # plt.show()
-            
-            moviesSelected = []
 
-            ## Need to resolve which movies came into the cluster in which user rated movie exists!!! ##
+            count = 0
+            clusterMovieSynopsis = []
+            userMovieSynopsis = []
+            dbCursor.execute("SELECT MovieID, Synopsis, Title FROM movies WHERE MovieID = ?;", (movie, ))
+            results = dbCursor.fetchall()
+            userMovieSynopsis = results[0][1]
+            # print([userMovieSynopsis])
 
             for clusterMovie in moviesSelected:
-                dbCursor.execute("SELECT MovieID, Synopsis FROM movies WHERE MovieID = ?;", (clusterMovie, ))
-                synopsis = dbCursor.fetchall()
-                synopsis = synopsis[0]
+                count += 1
+                dbCursor.execute("SELECT MovieID, Synopsis, Title FROM movies WHERE MovieID = ?;", (clusterMovie, ))
+                results = dbCursor.fetchall()
 
+                if count <= 1:
+                    continue
+
+                clusterMovieSynopsis = results[0][1]
+                rawText = []
+                rawText.append(userMovieSynopsis)
+                rawText.append(clusterMovieSynopsis)
+            
                 tfidfVectorizer = TfidfVectorizer(stop_words = 'english')
-                tfidfMatrix = tfidfVectorizer.fit_transform(synopsis)
+                tfidfMatrix = tfidfVectorizer.fit_transform(rawText)
+                # print(tfidfMatrix)
 
                 cosineSimilarity = linear_kernel(tfidfMatrix, tfidfMatrix)
+                # break
 
-                if cosineSimilarity > acceptingSimilarity:
+                if cosineSimilarity[0][1] > acceptingSimilarity:
+                    # print(cosineSimilarity[0][1])
                     recommendedMovies.append(clusterMovie)
-
+                
             # Testing to run once!
-            break;
+            # break;
         
         
         dbConnection.close()
         return recommendedMovies
 
+def recommendMoviesOnLastWatched(userID):
+    # Determining directory for accesing WatcherDB.db in /instance folder
+    currentDir = os.getcwd()
+    dbFile = 'WatcherDB.db'
+    dbFolder = 'instance'
+    dbPath = os.path.join(currentDir, dbFolder)
+    dbPath = os.path.join(dbPath, dbFile)
 
+    # Establishing DB connection
+    try:
+        dbConnection = sqlite3.connect(dbPath)
+        dbCursor = dbConnection.cursor()
+
+    except sqlite3.Error as error:
+        return 'Database connection failed!'
+    
+    # Variables needed by model
+    coldStart = None
+    minimumVotes = 50
+    recommendedMovies = []
+    userWatchedMovies = []
+    X = 0
+    Y = 0
+    allRelativeRatings = []
+    allAvgRatings = []
+    allMovieIDs = []
+    numClusters = 4
+    acceptingSimilarity = 0.015
+    #
+
+
+    # Checking for cold start problem
+    dbCursor.execute("SELECT isFirstTime FROM users WHERE id = ?;", (userID, ))
+    coldStart = dbCursor.fetchall()
+    coldStart = coldStart[0][0]
+
+
+    # Model Implementation
+    # Getting all movies user has watched
+    dbCursor.execute("SELECT watchedMovies FROM users WHERE id = ?", (userID, ))
+    userWatchedMovies = dbCursor.fetchall()
+    userWatchedMovies = convertTupleToList(userWatchedMovies)
+    print(userWatchedMovies.pop())
+    movie = userWatchedMovies.pop()
+    # iterating for each movie Collaborative then Content based filtering and generating recommendations based on each movie
+    
+    clusteringCoordinates = []
+
+    # Getting the rating user has given to it
+    dbCursor.execute("SELECT rating FROM user_rating WHERE userID = ? and movieID = ?", (userID, movie))
+    userMovieRating = dbCursor.fetchall()
+    userMovieRating = userMovieRating[0][0]
+    X = userMovieRating
+
+    # Getting the avg rating given to it
+    dbCursor.execute("SELECT AvgVote FROM movies WHERE movieID = ?", (movie, ))
+    userMovieAvgRating = dbCursor.fetchall()
+    userMovieAvgRating = userMovieAvgRating[0][0]
+    Y = userMovieAvgRating
+
+    # Creating coordinate for user movie
+    clusteringCoordinates.append((X, Y))
+    allMovieIDs.append(movie)
+
+    # Now, getting coordinates for all movies with similar genres
+    dbCursor.execute("SELECT Genres FROM movies WHERE MovieID = ?", (movie, ))
+    userMovieGenres = dbCursor.fetchall()
+    userMovieGenres = convertTupleToList(userMovieGenres)
+    # print(userMovieGenres)
+    
+    # Getting high rated movies for each genre
+    dbCursor.execute("SELECT MovieID, Genres, Title, AvgVote, VoteCount FROM movies WHERE VoteCount > ? ORDER BY AvgVote DESC", (minimumVotes, ))
+    results = dbCursor.fetchall()
+
+    for genre in userMovieGenres:
+        count = 0
+
+        for row in results:
+            moviesGenres = row[1].split(", ")
+
+            # Getting highest rated n movies for the genre
+            if set([genre]).issubset(set(moviesGenres)) and not(row[0] in userWatchedMovies) :
+                allRelativeRatings.append(relativeRating(row[3], userMovieRating))
+                allAvgRatings.append(row[3])
+                allMovieIDs.append(row[0])
+
+    # Creating coordinates for the choosen movies    
+    for x in range(len(allRelativeRatings)):
+        X = scaleRelativeRating(min(allRelativeRatings), max(allRelativeRatings), allRelativeRatings[x], 10)
+        Y = allAvgRatings[x]
+        clusteringCoordinates.append((X, Y))        
+    
+    # Converting coordinates from list of tuples to 2D numpy array
+    clusteringCoordinates = np.array(clusteringCoordinates)
+
+    
+    # plt.figure(figsize=(8, 6))
+    plt.scatter(clusteringCoordinates[:, 0], clusteringCoordinates[:, 1], color='blue', label='Movies from same genre')
+    plt.scatter(clusteringCoordinates[0][0], clusteringCoordinates[0][1], color='red')
+    # plt.annotate('User Movie', (clusteringCoordinates[0][0], clusteringCoordinates[0][1]), textcoords="offset points", xytext=(0, 10), ha='center')
+    # print(clusteringCoordinates[0])
+    
+
+    # Applying KMeans on cluster coordinates created
+    kmeans = KMeans(n_clusters = numClusters)
+    kmeans.fit(clusteringCoordinates)
+    clusterCenters = kmeans.cluster_centers_
+    plt.scatter(clusterCenters[:, 0], clusterCenters[:, 1], color='red', marker='x', s=100, label='Cluster Centers')
+    
+    for i in range(numClusters):
+        clusterPoints = clusteringCoordinates[kmeans.labels_ == i]
+        plt.scatter(clusterPoints[:, 0], clusterPoints[:, 1], label=f'Cluster {i+1}')
+    
+    movieClusterNo = 0
+    for i in range(numClusters):
+        clusterIndices = np.where(kmeans.labels_ == i)[0]
+        clusterPoints = clusteringCoordinates[clusterIndices]
+        # print(clusterPoints)
+        for j in clusterPoints:
+            if np.array_equal(clusteringCoordinates[0], j):
+                movieClusterNo = i
+
+    # print(movieClusterNo + 1)
+    clusterIndices = np.where(kmeans.labels_ == movieClusterNo)[0]
+    clusterPoints = clusteringCoordinates[clusterIndices]
+    moviesSelected = [allMovieIDs[i] for i in clusterIndices]
+    # print(moviesSelected)
+    # print(kmeans.labels_)
+
+    # plt.title('K-Means Clustering')
+    # plt.xlabel('X-coordinate')
+    # plt.ylabel('Y-coordinate')
+    # plt.legend()
+    # plt.show()
+
+    count = 0
+    clusterMovieSynopsis = []
+    userMovieSynopsis = []
+    dbCursor.execute("SELECT MovieID, Synopsis, Title FROM movies WHERE MovieID = ?;", (movie, ))
+    results = dbCursor.fetchall()
+    userMovieSynopsis = results[0][1]
+    # print([userMovieSynopsis])
+
+    for clusterMovie in moviesSelected:
+        count += 1
+        dbCursor.execute("SELECT MovieID, Synopsis, Title FROM movies WHERE MovieID = ?;", (clusterMovie, ))
+        results = dbCursor.fetchall()
+
+        if count <= 1:
+            continue
+
+        clusterMovieSynopsis = results[0][1]
+        rawText = []
+        rawText.append(userMovieSynopsis)
+        rawText.append(clusterMovieSynopsis)
+    
+        tfidfVectorizer = TfidfVectorizer(stop_words = 'english')
+        tfidfMatrix = tfidfVectorizer.fit_transform(rawText)
+        # print(tfidfMatrix)
+
+        cosineSimilarity = linear_kernel(tfidfMatrix, tfidfMatrix)
+        # break
+
+        if cosineSimilarity[0][1] > acceptingSimilarity:
+            # print(cosineSimilarity[0][1])
+            recommendedMovies.append(clusterMovie)
+        
+    # Testing to run once!
+    # break;
+
+
+    dbConnection.close()
+    return recommendedMovies
 
 
 # Implementation #
-print("Recommended Movies: ", recommendMoives(4))
+# print("Recommended Movies: ", len(recommendMoives(4)))
+# print(len(recommendMoviesOnLastWatched(4)))
